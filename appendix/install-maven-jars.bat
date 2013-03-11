98964286cad5cb03f51bf632119f55eadc78de03
Jaron <jaron.su@gmail.com>
2013-03-11 19:54:29 +0800

    initial commit

---
+++ appendix/install-maven-jars.bat
@@ -0,0 +1,5 @@
+@echo off
+mvn install:install-file -Dfile=ojdbc6.jar -DgroupId=oracle.jdbc -DartifactId=OracleJdbc -Dversion=11.2.0.1.0 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=pinyin4j-2.5.0.jar -DgroupId=com.pinyin -DartifactId=pinyin4j -Dversion=2.5.0 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=itextasian.jar -DgroupId=com.lowagie -DartifactId=itextasian -Dversion=1.4.2 -Dpackaging=jar -DgeneratePom=true
+
