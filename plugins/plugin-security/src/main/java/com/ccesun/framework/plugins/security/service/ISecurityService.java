1e94bcb4b25cbdf248f2fb619566b2df8998076a
Jaron <jaron.su@gmail.com>
2013-03-17 21:42:36 +0800

    Signed-off-by: Jaron <jaron.su@gmail.com>

--- plugins/plugin-security/src/main/java/com/ccesun/framework/plugins/security/service/ISecurityService.java
+++ plugins/plugin-security/src/main/java/com/ccesun/framework/plugins/security/service/ISecurityService.java
@@ -1,10 +1,12 @@
 package com.ccesun.framework.plugins.security.service;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.ccesun.framework.plugins.security.UserNotAvailableException;
 import com.ccesun.framework.plugins.security.UserNotFoundException;
 import com.ccesun.framework.plugins.security.WrongPasswordException;
+import com.ccesun.framework.plugins.security.WrongValidateCodeException;
 import com.ccesun.framework.plugins.security.domain.SecurityToken;
 
 public interface ISecurityService {
@@ -13,5 +15,12 @@ public interface ISecurityService {
 		(HttpServletResponse response, String[] usernames, String passwd, boolean rememberme)
 		throws UserNotFoundException, UserNotAvailableException, WrongPasswordException;
 	
+	SecurityToken login(HttpServletRequest request, HttpServletResponse response, String[] usernames,
+			String passwd, boolean rememberme, String validateCode)
+			throws UserNotFoundException, UserNotAvailableException,
+			WrongPasswordException, WrongValidateCodeException;
+	
 	void logout(HttpServletResponse response);
+
+	
 }
--- plugins/plugin-security/src/main/java/com/ccesun/framework/plugins/security/service/SecurityService.java
+++ plugins/plugin-security/src/main/java/com/ccesun/framework/plugins/security/service/SecurityService.java
@@ -2,21 +2,26 @@ package com.ccesun.framework.plugins.security.service;
 
 import java.util.Collection;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import com.ccesun.framework.plugins.security.PluginSecurityConstants;
 import com.ccesun.framework.plugins.security.SecurityTokenHolder;
 import com.ccesun.framework.plugins.security.UserNotAvailableException;
 import com.ccesun.framework.plugins.security.UserNotFoundException;
 import com.ccesun.framework.plugins.security.WrongPasswordException;
+import com.ccesun.framework.plugins.security.WrongValidateCodeException;
 import com.ccesun.framework.plugins.security.dao.ISecurityDao;
 import com.ccesun.framework.plugins.security.domain.IPermission;
 import com.ccesun.framework.plugins.security.domain.ISecurityUser;
 import com.ccesun.framework.plugins.security.domain.SecurityToken;
 import com.ccesun.framework.util.MD5;
+import com.ccesun.framework.util.StringUtils;
 
 @Service
 @Lazy
@@ -24,6 +29,29 @@ public class SecurityService implements ISecurityService {
 
 	@Autowired
 	public ISecurityDao iSecurityDao;
+	
+	@Override
+	public SecurityToken login(HttpServletRequest request, 
+							   HttpServletResponse response, 
+							   String[] usernames, 
+							   String passwd, 
+							   boolean rememberme,
+							   String validateCode) 
+						throws UserNotFoundException,
+							   UserNotAvailableException, 
+							   WrongPasswordException, WrongValidateCodeException{
+		
+		if (StringUtils.isBlank(validateCode))
+			throw new WrongValidateCodeException();
+		
+		HttpSession session = request.getSession();
+		String vCode = (String) session.getAttribute(PluginSecurityConstants.VALIDATE_CODE);
+		
+		if (!validateCode.equalsIgnoreCase(vCode))
+			throw new WrongValidateCodeException();
+		
+		return login(response, usernames, passwd, rememberme);
+	}
 	
 	@Override
 	public SecurityToken login(HttpServletResponse response, 
