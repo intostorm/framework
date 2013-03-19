7fdea4a6ecf489f3395c8151fc252b18becc7b79
Jaron <jaron.su@gmail.com>
2013-03-19 13:49:07 +0800

    Signed-off-by: Jaron <jaron.su@gmail.com>

---
+++ appendix/deploy.bat
@@ -0,0 +1,6 @@
+@echo off
+for %%i in (%0) do set dir=%%~dpi 
+cd /d %dir%
+cd ..
+call mvn deploy -DskipTests=true
+pause 
--- plugins/plugin-security/src/main/java/com/ccesun/framework/plugins/security/service/SecurityService.java
+++ plugins/plugin-security/src/main/java/com/ccesun/framework/plugins/security/service/SecurityService.java
@@ -29,61 +29,53 @@ public class SecurityService implements ISecurityService {
 
 	@Autowired
 	public ISecurityDao iSecurityDao;
-	
+
 	@Override
-	public SecurityToken login(HttpServletRequest request, 
-							   HttpServletResponse response, 
-							   String[] usernames, 
-							   String passwd, 
-							   boolean rememberme,
-							   String validateCode) 
-						throws UserNotFoundException,
-							   UserNotAvailableException, 
-							   WrongPasswordException, WrongValidateCodeException{
-		
+	public SecurityToken login(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			String[] usernames,
+			String passwd,
+			boolean rememberme,
+			String validateCode) throws UserNotFoundException, UserNotAvailableException, WrongPasswordException, WrongValidateCodeException {
+
 		if (StringUtils.isBlank(validateCode))
 			throw new WrongValidateCodeException();
-		
+
 		HttpSession session = request.getSession();
 		String vCode = (String) session.getAttribute(PluginSecurityConstants.VALIDATE_CODE);
-		
+
 		if (!validateCode.equalsIgnoreCase(vCode))
 			throw new WrongValidateCodeException();
-		
+
 		return login(response, usernames, passwd, rememberme);
 	}
-	
+
 	@Override
-	public SecurityToken login(HttpServletResponse response, 
-							   String[] usernames, 
-							   String passwd, 
-							   boolean rememberme) 
-						throws UserNotFoundException,
-							   UserNotAvailableException, 
-							   WrongPasswordException{
-		
+	public SecurityToken login(HttpServletResponse response, String[] usernames, String passwd, boolean rememberme)
+			throws UserNotFoundException, UserNotAvailableException, WrongPasswordException {
+
 		ISecurityUser user = iSecurityDao.getSecurityUser(usernames);
-		
+
 		if (user == null)
 			throw new UserNotFoundException();
-		
+
 		if (!user.isAvailable())
 			throw new UserNotAvailableException();
-		
-		if (!user.getPasswd().equals(passwd) 
-				&& !user.getPasswd().equals(MD5.toMD5(passwd))) 
+
+		if (!user.getPasswd().equals(passwd) && !user.getPasswd().equals(MD5.toMD5(passwd)))
 			throw new WrongPasswordException();
-		
+
 		Collection<IPermission> permissions = iSecurityDao.loadPermissions(user);
 		SecurityToken token = new SecurityToken(user, permissions);
-		
+
 		SecurityTokenHolder.setSecurityToken(response, token, rememberme);
 		return token;
 	}
-	
+
 	@Override
 	public void logout(HttpServletResponse response) {
 		SecurityTokenHolder.cleanSecurityToken(response);
 	}
-	
+
 }
