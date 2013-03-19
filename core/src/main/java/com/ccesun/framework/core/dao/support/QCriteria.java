6a63e96d2823bb26b5ea3141b5b11204c8b3b175
mawm <Administrator@ioio123>
2013-03-19 13:21:55 +0800

    扩展查询条件的解析操作

--- core/src/main/java/com/ccesun/framework/core/dao/support/QCriteria.java
+++ core/src/main/java/com/ccesun/framework/core/dao/support/QCriteria.java
@@ -1,13 +1,24 @@
 package com.ccesun.framework.core.dao.support;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+
+import com.ccesun.framework.util.DateUtils;
+import com.ccesun.framework.util.NumberUtils;
+import com.ccesun.framework.util.StringUtils;
 
 /**
  * 用于封装查询条件与参数，作为泛型Dao部分方法的参数，
- * <p>示例：
+ * <p>
+ * 示例：
  * 
- * <p> Foo类:<br>
+ * <p>
+ * Foo类:<br>
  * public class Foo {<br>
  * private String name;<br>
  * private String password;<br>
@@ -15,165 +26,371 @@ import java.util.List;
  * //省略getter和setter<br>
  * }
  * 
- * <p>public class Bar {<br>
+ * <p>
+ * public class Bar {<br>
  * private String name;<br>
  * //省略getter和setter<br>
  * }
  * 
- * <p>QCriteria criteria = new QCriteria();<br>
+ * <p>
+ * QCriteria criteria = new QCriteria();<br>
  * criteria<br>
  * .addEntry("name", Op.EQ, "name1")<br>
- * .where("o.password like ? and o.password not like ?", "pass".concat("%"), "password1".concat("%"))<br>
+ * .where("o.password like ? and o.password not like ?", "pass".concat("%"),
+ * "password1".concat("%"))<br>
  * .innerFetch("bar"); //内联接Bar属性，可选的联接方式有innerFetch, leftFetch, rightFetch<br>
  * 
- * <p>List<Foo> = simpleDao.find(QCriteria criteria);
- *
+ * <p>
+ * List<Foo> = simpleDao.find(QCriteria criteria);
+ * 
  * @author Jaron
- *
+ * 
  */
 public class QCriteria {
+	/**
+	 * 把Value转换成真实的类型
+	 * 
+	 * @param fieldName
+	 * @param value
+	 * @return
+	 */
+	public static Object converValue(String fieldName, String value) {
 
-    private List<ParamEntry> entries = new ArrayList<ParamEntry>();
+		if (fieldName.endsWith(SearchForm.TYPE_DATE)) {
+			try {
+				return new SimpleDateFormat(DateUtils.PATTERN_DATE)
+						.parse(value);
+			} catch (ParseException e) {
+			}
+		}
 
-    private List<Fetch> fetchList = new ArrayList<Fetch>();
-    
-    private List<WhereEntry> whereEntries = new ArrayList<WhereEntry>();
-    
-    private int firstResult;
-    private int maxResults;
-    
-    /**
-     * 得到所有通过addEntry()方法添加的查询条件
-     * @return 查询条件的集合
-     */
-    public List<ParamEntry> getEntries() {
-        return entries;
-    }
+		else if (fieldName.endsWith(SearchForm.TYPE_INT)) {
+			return new Integer(String.valueOf(value));
+		}
 
-    /**
-     * 添加查询条件
-     * <p>示例：
-     * <p>QCriteria criteria = new QCriteria();<br>
-     * criteria<br>
-     * .addEntry("name", Op.EQ, "name1");<br>
-     * @param paramName 查询对象的属性名
-     * @param queryCase 操作符
-     * @param value 属性值
-     * @return 对象本身，为了形成链式操作
-     */
-    public QCriteria addEntry(String paramName, String queryCase, Object value) {
-    	ParamEntry queryParamEntry = new ParamEntry(paramName, queryCase, value);
-    	this.entries.add(queryParamEntry);
-    	return this;
-    }
-    
-    /**
-     * 设为第一个条件
-     * @deprecated 因为不会影响查询语句的优化
-     * @param paramName 查询对象的属性名
-     * @param queryCase 操作符
-     * @param value 属性值
-     * @return 对象本身，为了形成链式操作
-     */
-    public QCriteria addEntryToFirst(String paramName, String queryCase, Object value) {
-        ParamEntry queryParamEntry = new ParamEntry(paramName, queryCase, value);
-        this.entries.add(0, queryParamEntry);
-        return this;
-    }
+		else if (fieldName.endsWith(SearchForm.TYPE_STRING)) {
+			return String.valueOf(value);
+		} else if (fieldName.endsWith(SearchForm.TYPE_SPLITTXT1)
+				|| fieldName.endsWith(SearchForm.TYPE_SPLITTXT2)) {
 
-    /**
-     * 得到由innerFetch(), leftFetch, rightFetch() 添加的联接信息
-     * @return 联接信息的集合
-     */
-    public List<Fetch> getFetchList() {
-        return fetchList;
-    }
+			// 判断用哪个字符进行截取!
+			String separatorChars = ";";
+			if (fieldName.endsWith(SearchForm.TYPE_SPLITTXT1)) {
+				separatorChars = ";";
+			} else if (fieldName.endsWith(SearchForm.TYPE_SPLITTXT2)) {
+				separatorChars = ",";
+			} else {
+				separatorChars = ";";
+			}
 
-    /**
-     * 添加一个内联接信息并抓取目标对象
-     * @param targetObjectName 属性
-     * @return 对象本身，为了形成链式操作
-     */
-    public QCriteria innerFetch(String targetObjectName) {
-        this.fetchList.add(Fetch.innerFetch(targetObjectName));
-        return this;
-    }
-    
-    /**
-     * 添加一个左外联接信息并抓取目标对象
-     * @param targetObjectName 属性
-     * @return 对象本身，为了形成链式操作
-     */
-    public QCriteria leftFetch(String targetObjectName) {
-        this.fetchList.add(Fetch.leftFetch(targetObjectName));
-        return this;
-    }
-    
-    /**
-     * 添加一个右外联接信息并抓取目标对象
-     * @param targetObjectName 属性
-     * @return 对象本身，为了形成链式操作
-     */
-    public QCriteria rightFetch(String targetObjectName) {
-        this.fetchList.add(Fetch.rightFetch(targetObjectName));
-        return this;
-    }
-    
-    /**
-     * 添加一个内联接信息
-     * @param targetObjectName 属性
-     * @return 对象本身，为了形成链式操作
-     */
-    public QCriteria innerJoin(String targetObjectName) {
-        this.fetchList.add(Fetch.innerJoin(targetObjectName));
-        return this;
-    }
-    
-    /**
-     * 添加一个左外联接信息
-     * @param targetObjectName 属性
-     * @return 对象本身，为了形成链式操作
-     */
-    public QCriteria leftJoin(String targetObjectName) {
-        this.fetchList.add(Fetch.leftJoin(targetObjectName));
-        return this;
-    }
-    
-    /**
-     * 添加一个右外联接信息
-     * @param targetObjectName 属性
-     * @return 对象本身，为了形成链式操作
-     */
-    public QCriteria rightJoin(String targetObjectName) {
-        this.fetchList.add(Fetch.rightJoin(targetObjectName));
-        return this;
-    }
-	
-    /**
-     * 添加一个或多个查询条件，与addEntry作用相似，不同处在于where()方法内指定属性时要由一个前缀"o."，
-     * 而且where()方法内可以一次指定多个条件，能适应更多的情况。比如 OR条件, 使用数据库函数等。
-     * 
-     * <p>示例：
-     * <p>QCriteria criteria = new QCriteria();<br>
-     * criteria<br>
-     * .where("o.name = ? or o.name = ?", "name1", "name2");<br>
-     * 
-     * @param whereClause 查询条件
-     * @param values 查询参数
-     * @return 对象本身，为了形成链式操作
-     */
+			// 对内容进行split
+			String[] split = StringUtils.split(value, separatorChars);
+			// 把内容转换为List对象
+			return Arrays.asList(split);
+
+		} else if (fieldName.endsWith(SearchForm.TYPE_SPLIT_INT1)
+				|| fieldName.endsWith(SearchForm.TYPE_SPLIT_INT2)) {
+
+			// 判断用哪个字符进行截取!
+			String separatorChars = ";";
+			if (fieldName.endsWith(SearchForm.TYPE_SPLIT_INT1)) {
+				separatorChars = ";";
+			} else if (fieldName.endsWith(SearchForm.TYPE_SPLIT_INT2)) {
+				separatorChars = ",";
+			} else {
+				separatorChars = ";";
+			}
+
+			// 对内容进行split
+			String[] split = StringUtils.split(value, separatorChars);
+			Integer[] integerArray = NumberUtils.toIntegerArray(split);
+			// 把内容转换为List对象
+			return Arrays.asList(integerArray);
+
+		}
+
+		return String.valueOf(value);
+	}
+
+	/**
+	 * 解析一个表达式
+	 * 
+	 * @param name
+	 * @param value
+	 * @return
+	 * @author mawm at 2013-3-19 下午1:16:12
+	 */
+	public static ParamEntry parseOne(String name, String value) {
+
+		if (StringUtils.isBlank(value))
+			return null;
+
+		ParamEntry e = null;
+		if (name.contains(SearchForm.OP_GT_SUFFIX)) {
+			String fieldName = StringUtils.substringBefore(name,
+					SearchForm.OP_GT_SUFFIX);
+			Object realValue = converValue(name, value);
+			e = new ParamEntry(fieldName, ">", realValue);
+		} else if (name.contains(SearchForm.OP_LT_SUFFIX)) {
+			String fieldName = StringUtils.substringBefore(name,
+					SearchForm.OP_LT_SUFFIX);
+			Object realValue = converValue(name, value);
+			e = new ParamEntry(fieldName, "<", realValue);
+		} else if (name.contains(SearchForm.OP_LIKE_SUFFIX)) {
+			String fieldName = StringUtils.substringBefore(name,
+					SearchForm.OP_LIKE_SUFFIX);
+			Object realValue = converValue(name, value);
+			e = new ParamEntry(fieldName, "like", realValue + "%");
+		} else if (name.contains(SearchForm.OP_BOTH_LIKE_SUFFIX)) {
+			String fieldName = StringUtils.substringBefore(name,
+					SearchForm.OP_BOTH_LIKE_SUFFIX);
+			Object realValue = converValue(name, value);
+			e = new ParamEntry(fieldName, "like", "%" + realValue + "%");
+		} else if (name.contains(SearchForm.OP_EQ_SUFFIX)) {
+			String fieldName = StringUtils.substringBefore(name,
+					SearchForm.OP_EQ_SUFFIX);
+			Object realValue = converValue(name, value);
+			e = new ParamEntry(fieldName, "=", realValue);
+		} else if (name.contains(SearchForm.OP_NOT_EQ_SUFFIX)) {
+			String fieldName = StringUtils.substringBefore(name,
+					SearchForm.OP_NOT_EQ_SUFFIX);
+			Object realValue = converValue(name, value);
+			e = new ParamEntry(fieldName, "<>", realValue);
+		} else if (name.contains(SearchForm.OP_EQ_AND_GT_SUFFIX)) {
+			String fieldName = StringUtils.substringBefore(name,
+					SearchForm.OP_EQ_AND_GT_SUFFIX);
+			Object realValue = converValue(name, value);
+			e = new ParamEntry(fieldName, ">=", realValue);
+		} else if (name.contains(SearchForm.OP_EQ_AND_LT_SUFFIX)) {
+			String fieldName = StringUtils.substringBefore(name,
+					SearchForm.OP_EQ_AND_LT_SUFFIX);
+			Object realValue = converValue(name, value);
+			e = new ParamEntry(fieldName, "<=", realValue);
+
+		} else if (name.contains(SearchForm.OP_IN_SUFFIX)) {
+			// 进行in操作!
+			String fieldName = StringUtils.substringBefore(name,
+					SearchForm.OP_IN_SUFFIX);
+			Object realValue = converValue(name, value);
+			e = new ParamEntry(fieldName, QCriteria.Op.IN, realValue);
+		} else {
+			return null;
+		}
+		return e;
+
+	}
+
+	/**
+	 * 解释SearchForm生成查询条件
+	 * 
+	 * @param searchForm
+	 * @return
+	 */
+	public static QCriteria parseForm(SearchForm searchForm) {
+
+		QCriteria criteria = new QCriteria();
+		Map<String, String> form = searchForm.getForm();
+
+		for (Iterator<Map.Entry<String, String>> iterator = form.entrySet()
+				.iterator(); iterator.hasNext();) {
+			Map.Entry<String, String> entry = iterator.next();
+			String name = entry.getKey();
+			String value = entry.getValue();
+
+			if (StringUtils.isBlank(value))
+				continue;
+			// 解析一个查询条件
+			ParamEntry e = parseOne(name, value);
+			if (e != null) {
+				criteria.addEntry(e);
+			}
+		}
+
+		return criteria;
+	}
+
+	private List<ParamEntry> entries = new ArrayList<ParamEntry>();
+
+	private List<Fetch> fetchList = new ArrayList<Fetch>();
+
+	private List<WhereEntry> whereEntries = new ArrayList<WhereEntry>();
+
+	private int firstResult;
+	private int maxResults;
+
+	/**
+	 * 得到所有通过addEntry()方法添加的查询条件
+	 * 
+	 * @return 查询条件的集合
+	 */
+	public List<ParamEntry> getEntries() {
+		return entries;
+	}
+
+	/**
+	 * 添加查询条件
+	 * <p>
+	 * 示例：
+	 * <p>
+	 * QCriteria criteria = new QCriteria();<br>
+	 * criteria<br>
+	 * .addEntry("name", Op.EQ, "name1");<br>
+	 * 
+	 * @param paramName
+	 *            查询对象的属性名
+	 * @param queryCase
+	 *            操作符
+	 * @param value
+	 *            属性值
+	 * @return 对象本身，为了形成链式操作
+	 */
+	public QCriteria addEntry(String paramName, String queryCase, Object value) {
+		ParamEntry entry = new ParamEntry(paramName, queryCase, value);
+		return addEntry(entry);
+	}
+
+	/**
+	 * 添加一个已经封装好的查询条件对象!
+	 * 
+	 * @param entry
+	 * @return
+	 * @author mawm at 2013-3-19 下午12:19:47
+	 */
+	public QCriteria addEntry(ParamEntry entry) {
+		this.entries.add(entry);
+		return this;
+	}
+
+	/**
+	 * 设为第一个条件
+	 * 
+	 * @deprecated 因为不会影响查询语句的优化
+	 * @param paramName
+	 *            查询对象的属性名
+	 * @param queryCase
+	 *            操作符
+	 * @param value
+	 *            属性值
+	 * @return 对象本身，为了形成链式操作
+	 */
+	public QCriteria addEntryToFirst(String paramName, String queryCase,
+			Object value) {
+		ParamEntry queryParamEntry = new ParamEntry(paramName, queryCase, value);
+		this.entries.add(0, queryParamEntry);
+		return this;
+	}
+
+	/**
+	 * 得到由innerFetch(), leftFetch, rightFetch() 添加的联接信息
+	 * 
+	 * @return 联接信息的集合
+	 */
+	public List<Fetch> getFetchList() {
+		return fetchList;
+	}
+
+	/**
+	 * 添加一个内联接信息并抓取目标对象
+	 * 
+	 * @param targetObjectName
+	 *            属性
+	 * @return 对象本身，为了形成链式操作
+	 */
+	public QCriteria innerFetch(String targetObjectName) {
+		this.fetchList.add(Fetch.innerFetch(targetObjectName));
+		return this;
+	}
+
+	/**
+	 * 添加一个左外联接信息并抓取目标对象
+	 * 
+	 * @param targetObjectName
+	 *            属性
+	 * @return 对象本身，为了形成链式操作
+	 */
+	public QCriteria leftFetch(String targetObjectName) {
+		this.fetchList.add(Fetch.leftFetch(targetObjectName));
+		return this;
+	}
+
+	/**
+	 * 添加一个右外联接信息并抓取目标对象
+	 * 
+	 * @param targetObjectName
+	 *            属性
+	 * @return 对象本身，为了形成链式操作
+	 */
+	public QCriteria rightFetch(String targetObjectName) {
+		this.fetchList.add(Fetch.rightFetch(targetObjectName));
+		return this;
+	}
+
+	/**
+	 * 添加一个内联接信息
+	 * 
+	 * @param targetObjectName
+	 *            属性
+	 * @return 对象本身，为了形成链式操作
+	 */
+	public QCriteria innerJoin(String targetObjectName) {
+		this.fetchList.add(Fetch.innerJoin(targetObjectName));
+		return this;
+	}
+
+	/**
+	 * 添加一个左外联接信息
+	 * 
+	 * @param targetObjectName
+	 *            属性
+	 * @return 对象本身，为了形成链式操作
+	 */
+	public QCriteria leftJoin(String targetObjectName) {
+		this.fetchList.add(Fetch.leftJoin(targetObjectName));
+		return this;
+	}
+
+	/**
+	 * 添加一个右外联接信息
+	 * 
+	 * @param targetObjectName
+	 *            属性
+	 * @return 对象本身，为了形成链式操作
+	 */
+	public QCriteria rightJoin(String targetObjectName) {
+		this.fetchList.add(Fetch.rightJoin(targetObjectName));
+		return this;
+	}
+
+	/**
+	 * 添加一个或多个查询条件，与addEntry作用相似，不同处在于where()方法内指定属性时要由一个前缀"o."，
+	 * 而且where()方法内可以一次指定多个条件，能适应更多的情况。比如 OR条件, 使用数据库函数等。
+	 * 
+	 * <p>
+	 * 示例：
+	 * <p>
+	 * QCriteria criteria = new QCriteria();<br>
+	 * criteria<br>
+	 * .where("o.name = ? or o.name = ?", "name1", "name2");<br>
+	 * 
+	 * @param whereClause
+	 *            查询条件
+	 * @param values
+	 *            查询参数
+	 * @return 对象本身，为了形成链式操作
+	 */
 	public QCriteria where(String whereClause, Object... values) {
 		ArrayList<Object> params = new ArrayList<Object>(values.length);
-        for (Object value : values) {
-        	params.add(value);
+		for (Object value : values) {
+			params.add(value);
 		}
-        WhereEntry whereEntry = new WhereEntry(whereClause, params);
-        this.whereEntries.add(whereEntry);
-        return this;
-    }
-	
+		WhereEntry whereEntry = new WhereEntry(whereClause, params);
+		this.whereEntries.add(whereEntry);
+		return this;
+	}
+
 	/**
 	 * 判断是否含有由where()方法添加的查询条件
+	 * 
 	 * @return 对象本身，为了形成链式操作
 	 */
 	public boolean hasWhereClause() {
@@ -182,33 +399,38 @@ public class QCriteria {
 
 	/**
 	 * 清除所有已添加的查询条件，联接信息
+	 * 
 	 * @return 对象本身，为了形成链式操作
 	 */
-    public QCriteria clear() {
-        this.entries.clear();
-        this.fetchList.clear();
-        this.whereEntries.clear();
-        return this;
-    }
+	public QCriteria clear() {
+		this.entries.clear();
+		this.fetchList.clear();
+		this.whereEntries.clear();
+		return this;
+	}
 
-    /**
-     * 得到查询结果的起始偏移值，用于分页操作
-     * @return 起始偏移值
-     */
-    public int getFirstResult() {
+	/**
+	 * 得到查询结果的起始偏移值，用于分页操作
+	 * 
+	 * @return 起始偏移值
+	 */
+	public int getFirstResult() {
 		return firstResult;
 	}
 
-    /**
-     * 设置结果的起始偏移值，用于分页操作
-     * @param firstResult 起始偏移值
-     */
+	/**
+	 * 设置结果的起始偏移值，用于分页操作
+	 * 
+	 * @param firstResult
+	 *            起始偏移值
+	 */
 	public void setFirstResult(int firstResult) {
 		this.firstResult = firstResult;
 	}
 
 	/**
 	 * 得到查询结果的数量，用于分页操作
+	 * 
 	 * @return 数量
 	 */
 	public int getMaxResults() {
@@ -217,7 +439,9 @@ public class QCriteria {
 
 	/**
 	 * 设置查询结果的数量，用于分页操作
-	 * @param maxResults 数量
+	 * 
+	 * @param maxResults
+	 *            数量
 	 */
 	public void setMaxResults(int maxResults) {
 		this.maxResults = maxResults;
@@ -225,6 +449,7 @@ public class QCriteria {
 
 	/**
 	 * 得到由where()方法添加的查询条件集合
+	 * 
 	 * @return
 	 */
 	public List<WhereEntry> getWhereEntries() {
@@ -233,7 +458,7 @@ public class QCriteria {
 
 	public static class WhereEntry {
 		private String whereClause;
-		
+
 		private List<Object> whereParams;
 
 		public WhereEntry(String whereClause, List<Object> whereParams) {
@@ -257,62 +482,62 @@ public class QCriteria {
 		public void setWhereParams(List<Object> whereParams) {
 			this.whereParams = whereParams;
 		}
-		
+
 	}
 
 	public static class ParamEntry {
 
-        private String paramName;
-        
-        private String queryCase;
-        
-        private Object value;
+		private String paramName;
 
-        public ParamEntry() {
-        }
+		private String queryCase;
 
-        public ParamEntry(String paramName, String queryCase, Object value) {
-            this.paramName = paramName;
-            this.queryCase = queryCase;
-            this.value = value;
-        }
+		private Object value;
 
-        public String getParamName() {
-            return paramName;
-        }
+		public ParamEntry() {
+		}
 
-        public void setParamName(String paramName) {
-            this.paramName = paramName;
-        }
+		public ParamEntry(String paramName, String queryCase, Object value) {
+			this.paramName = paramName;
+			this.queryCase = queryCase;
+			this.value = value;
+		}
 
-    	public String getQueryCase() {
-    		return queryCase;
-    	}
+		public String getParamName() {
+			return paramName;
+		}
 
-    	public void setQueryCase(String queryCase) {
-    		this.queryCase = queryCase;
-    	}
+		public void setParamName(String paramName) {
+			this.paramName = paramName;
+		}
 
-    	public Object getValue() {
-    		return value;
-    	}
+		public String getQueryCase() {
+			return queryCase;
+		}
 
-    	public void setValue(Object value) {
-    		this.value = value;
-    	}
-        
-    }
-    
-    public static class Op {
-    	public static final String EQ = "=";
-    	/** 仅对NULL值使用 */
-    	public static final String IS = "IS";
-    	public static final String LT = "<";
-    	public static final String GT = ">";
-    	public static final String LTEQ = "<=";
-    	public static final String GTEQ = ">=";
-    	public static final String NE = "<>";
-    	public static final String LIKE = "LIKE";
-    	public static final String IN = "IN";
-    }
+		public void setQueryCase(String queryCase) {
+			this.queryCase = queryCase;
+		}
+
+		public Object getValue() {
+			return value;
+		}
+
+		public void setValue(Object value) {
+			this.value = value;
+		}
+
+	}
+
+	public static class Op {
+		public static final String EQ = "=";
+		/** 仅对NULL值使用 */
+		public static final String IS = "IS";
+		public static final String LT = "<";
+		public static final String GT = ">";
+		public static final String LTEQ = "<=";
+		public static final String GTEQ = ">=";
+		public static final String NE = "<>";
+		public static final String LIKE = "LIKE";
+		public static final String IN = "IN";
+	}
 }
--- core/src/main/java/com/ccesun/framework/core/dao/support/SearchForm.java
+++ core/src/main/java/com/ccesun/framework/core/dao/support/SearchForm.java
@@ -9,7 +9,7 @@ import com.ccesun.framework.util.StringUtils;
 public class SearchForm implements Serializable, Cloneable {
 
 	private static final long serialVersionUID = 1L;
-    
+
 	public static final String OP_LIKE_SUFFIX = "_lk";
 	public static final String OP_BOTH_LIKE_SUFFIX = "_blk";
 	public static final String OP_EQ_SUFFIX = "_eq";
@@ -18,20 +18,53 @@ public class SearchForm implements Serializable, Cloneable {
 	public static final String OP_LT_SUFFIX = "_lt";
 	public static final String OP_EQ_AND_GT_SUFFIX = "_egt";
 	public static final String OP_EQ_AND_LT_SUFFIX = "_elt";
-	
+
+	/**
+	 * 用来描述in操作的后缀,需要和value进行配合,来截取出具体的 value值, 默认的字符串的split是';'
+	 * 
+	 * @author mawm at 2013-3-19 下午12:21:22
+	 * @see TYPE_SPLITTXT
+	 */
+	public static final String OP_IN_SUFFIX = "_in";
+
 	public static final String TYPE_STRING = "_string";
 	public static final String TYPE_INT = "_int";
 	public static final String TYPE_DATE = "_date";
 
+	/**
+	 * 对 数据进行通过';'进行split截取操作,得到一个字符串数组!
+	 * 
+	 * @author mawm at 2013-3-19 下午12:22:31
+	 */
+	public static final String TYPE_SPLITTXT1 = "_split1";
+	/**
+	 * 对 数据进行通过','进行split截取操作,得到一个字符串数组!
+	 * 
+	 * @author mawm at 2013-3-19 下午12:22:31
+	 */
+	public static final String TYPE_SPLITTXT2 = "_split2";
+	/**
+	 * 对 数据进行通过';'进行split截取操作,得到一个Integer数组!
+	 * 
+	 * @author mawm at 2013-3-19 下午12:22:31
+	 */
+	public static final String TYPE_SPLIT_INT1 = "_splitint1";
+	/**
+	 * 对 数据进行通过','进行split截取操作,得到一个Integer数组!
+	 * 
+	 * @author mawm at 2013-3-19 下午12:22:31
+	 */
+	public static final String TYPE_SPLIT_INT2 = "_splitint2";
+
 	public static final String TYPE_DEFAULT = TYPE_STRING;
-	
+
 	private Map<String, String> form = new HashMap<String, String>();
-	
+
 	private PageRequest pageRequest = new PageRequest();
 
 	private String orderBy;
 	private String order;
-	
+
 	public Map<String, String> getForm() {
 		return form;
 	}
@@ -39,9 +72,9 @@ public class SearchForm implements Serializable, Cloneable {
 	public void setForm(Map<String, String> form) {
 		this.form = form;
 	}
-	
+
 	public PageRequest getPageRequest() {
-		
+
 		if (!StringUtils.isBlank(orderBy)) {
 			if (!StringUtils.isBlank(order)) {
 				pageRequest.setSort(new Sort().order(orderBy, order));
@@ -49,18 +82,18 @@ public class SearchForm implements Serializable, Cloneable {
 				pageRequest.setSort(new Sort().asc(orderBy));
 			}
 		}
-		
+
 		return pageRequest;
 	}
 
 	public void setPageRequest(PageRequest pageRequest) {
 		this.pageRequest = pageRequest;
 	}
-	
+
 	public void setPageNo(int pageNo) {
 		this.pageRequest.setPageNo(pageNo);
-	} 
-	
+	}
+
 	public void setPageSize(int pageSize) {
 		this.pageRequest.setPageSize(pageSize);
 	}
@@ -81,20 +114,22 @@ public class SearchForm implements Serializable, Cloneable {
 		this.order = order;
 	}
 
-	public SearchForm addFormEntry(String key, String value){
+	public SearchForm addFormEntry(String key, String value) {
 		form.put(key, value);
 		return this;
 	}
-	
+
 	public SearchForm clone() {
 		SearchForm result = new SearchForm();
-		result.setOrderBy(this.getOrderBy() == null ? null : new String(this.getOrderBy()));
-		result.setOrder(this.getOrder() == null ? null : new String(this.getOrder()));
-		
+		result.setOrderBy(this.getOrderBy() == null ? null : new String(this
+				.getOrderBy()));
+		result.setOrder(this.getOrder() == null ? null : new String(this
+				.getOrder()));
+
 		PageRequest pageRequest = new PageRequest();
 		pageRequest.setPageNo(getPageRequest().getPageNo());
 		pageRequest.setPageSize(getPageRequest().getPageSize());
-		
+
 		result.setPageRequest(pageRequest);
 		result.setForm(new HashMap<String, String>(this.getForm()));
 
