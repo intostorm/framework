a04b101156904213ac65e37ee5ec894e49e4065c
mawm <Administrator@ioio123>
2013-04-02 16:55:53 +0800

    屏蔽了如果 中文中有标点等字符,出现空指针的bug

--- core/src/main/java/com/ccesun/framework/util/CnSpellUtils.java
+++ core/src/main/java/com/ccesun/framework/util/CnSpellUtils.java
@@ -10,46 +10,54 @@ import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombi
 public class CnSpellUtils {
 
 	private static final HanyuPinyinOutputFormat hanyuPinyinOutputFormat;
-	
+
 	static {
 		hanyuPinyinOutputFormat = new HanyuPinyinOutputFormat();
 		hanyuPinyinOutputFormat.setCaseType(HanyuPinyinCaseType.UPPERCASE);
 		hanyuPinyinOutputFormat.setVCharType(HanyuPinyinVCharType.WITH_V);
 		hanyuPinyinOutputFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
 	}
-	
+
 	public static String getFullCnSpell(String source) {
 		StringBuilder result = new StringBuilder();
 		char[] sourceCharArray = source.toCharArray();
-		
+
 		for (char c : sourceCharArray) {
 			try {
-				String[] tmpArray = PinyinHelper.toHanyuPinyinStringArray(c, hanyuPinyinOutputFormat);
-				result.append(tmpArray[0]);
+				String[] tmpArray = PinyinHelper.toHanyuPinyinStringArray(c,
+						hanyuPinyinOutputFormat);
+				if (tmpArray != null && tmpArray.length > 0)
+					result.append(tmpArray[0]);
 			} catch (BadHanyuPinyinOutputFormatCombination e) {
 			}
 		}
-		
+
 		return result.toString();
 	}
-	
+
 	public static String getFirstCnSpell(String source) {
 		StringBuilder result = new StringBuilder();
 		char[] sourceCharArray = source.toCharArray();
-		
+
 		for (char c : sourceCharArray) {
 			try {
-				if(c > 128){
-					String[] tmpArray = PinyinHelper.toHanyuPinyinStringArray(c, hanyuPinyinOutputFormat);
-					result.append(tmpArray[0].charAt(0));
-				}else{
+				if (c > 128) {
+					String[] tmpArray = PinyinHelper.toHanyuPinyinStringArray(
+							c, hanyuPinyinOutputFormat);
+					if (tmpArray != null && tmpArray.length > 0) {
+						String v = tmpArray[0];
+						if (v.length() > 0)
+							result.append(v.charAt(0));
+
+					}
+				} else {
 					result.append(c);
 				}
 			} catch (BadHanyuPinyinOutputFormatCombination e) {
 			}
 		}
-		
+
 		return result.toString();
 	}
-	
+
 }
