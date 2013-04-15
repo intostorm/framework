6fef2150ad26a145f8808a0ea4c41d2413b6f496
mawm <Administrator@ioio123>
2013-04-15 11:11:27 +0800

    增加删除操作

--- plugins/plugin-search/src/main/java/com/ccesun/framework/plugins/search/SearchUtils.java
+++ plugins/plugin-search/src/main/java/com/ccesun/framework/plugins/search/SearchUtils.java
@@ -1,6 +1,7 @@
 package com.ccesun.framework.plugins.search;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
@@ -13,6 +14,7 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
@@ -21,11 +23,13 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopScoreDocCollector;
+import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.NIOFSDirectory;
 
 import com.ccesun.framework.core.AppContext;
 import com.ccesun.framework.core.dao.support.Page;
 import com.ccesun.framework.core.dao.support.PageRequest;
+import com.ccesun.framework.plugins.aop.SearchIndexAdvice;
 import com.ccesun.framework.util.BeanUtils;
 import com.ccesun.framework.util.StringUtils;
 
@@ -79,7 +83,8 @@ public class SearchUtils {
 	/**
 	 * @param pageRequest
 	 * @param clazz
-	 * @param query 可以自行构造多种查询规则进行检索
+	 * @param query
+	 *            可以自行构造多种查询规则进行检索
 	 * @return
 	 * @author mawm at 2013-4-12 下午4:11:49
 	 */
@@ -89,11 +94,15 @@ public class SearchUtils {
 		int pageSize = pageRequest.getPageSize();
 
 		TopScoreDocCollector topCollector;
+		IndexSearcher searcher = null;
 		List<T> content;
+		IndexReader open = null;
+		NIOFSDirectory directory = null;
 		try {
 			File indexDir = getSearchIndexDir();
-			IndexSearcher searcher = new IndexSearcher(
-					IndexReader.open(new NIOFSDirectory(indexDir)));
+			directory = new NIOFSDirectory(indexDir);
+			open = IndexReader.open(directory);
+			searcher = new IndexSearcher(open);
 
 			topCollector = null;
 			content = new ArrayList<T>();
@@ -114,6 +123,22 @@ public class SearchUtils {
 		} catch (Exception e) {
 			logger.error("", e);
 			return new Page<T>(new ArrayList<T>(), pageRequest, 0);
+		} finally {
+			try {
+				directory.close();
+			} catch (Exception e2) {
+				logger.error("", e2);
+			}
+			try {
+				open.close();
+			} catch (IOException e1) {
+				logger.error("", e1);
+			}
+			try {
+				searcher.close();
+			} catch (IOException e) {
+				logger.error("", e);
+			}
 		}
 
 	}
@@ -175,5 +200,31 @@ public class SearchUtils {
 		}
 
 		return query;
+	}
+
+	public static <T> void deleteDocuments(Query query, Class<T> clazz) {
+		SearchIndexAdvice bean = AppContext.getInstance()
+				.getApplicationContext().getBean(SearchIndexAdvice.class);
+		if (bean != null) {
+			bean.deleteDocuments(query);
+		}
+	}
+
+	public static <T> void deleteDocuments(Term term, Class<T> clazz) {
+		SearchIndexAdvice bean = AppContext.getInstance()
+				.getApplicationContext().getBean(SearchIndexAdvice.class);
+		if (bean != null) {
+			bean.deleteDocuments(term);
+		}
+	}
+	
+	/**
+	 * @throws IOException
+	 * @author mawm at 2013-4-15 上午9:03:47
+	 */
+	public static void unlock(Directory indexDirectory) throws IOException {
+		if (IndexWriter.isLocked(indexDirectory)) {
+			IndexWriter.unlock(indexDirectory);
+		}
 	}
 }
