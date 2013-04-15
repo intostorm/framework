2fcd5b4ca63a93b66e853273c9e20debd19fa268
Jaron <jaron.su@gmail.com>
2013-04-15 09:02:53 +0800

    在framework/appendix里加入了第三方包ik-analyzer
    Signed-off-by: Jaron <jaron.su@gmail.com>

--- appendix/install-maven-jars.bat
+++ appendix/install-maven-jars.bat
@@ -4,4 +4,5 @@ cd /d %dir%
 call mvn install:install-file -Dfile=OracleJdbc-11.2.0.1.0.jar -DgroupId=oracle.jdbc -DartifactId=OracleJdbc -Dversion=11.2.0.1.0 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=pinyin4j-2.5.0.jar -DgroupId=com.pinyin -DartifactId=pinyin4j -Dversion=2.5.0 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=itextasian-1.4.2.jar -DgroupId=com.lowagie -DartifactId=itextasian -Dversion=1.4.2 -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=ik-analyzer-2012_u6.jar -DgroupId=org.wltea.ik-analyzer -DartifactId=ik-analyzer -Dversion=2012_u6 -Dpackaging=jar -DgeneratePom=true
 pause 
